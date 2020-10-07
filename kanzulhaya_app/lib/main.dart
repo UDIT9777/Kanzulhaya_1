42fdd22b8951e7c998a9e63525a02fededf54106
Udit Kumar Panda <uditcsevss.97777@gmail.com>
2020-10-07 22:54:20 +0530

    initial commit

--- kanzulhaya_app/lib/main.dart
+++ kanzulhaya_app/lib/main.dart
@@ -2,12 +2,16 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:flutter/services.dart';
+import 'dart:async';
+
 
 void main(){
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     statusBarColor: Colors.black,
   ));
   runApp(MyApp());}
+
+
 
 
 class MyApp extends StatelessWidget {
@@ -32,17 +36,35 @@ class WebViewLoad extends StatefulWidget {
 
 }
 
+WebViewController controllerGlobal;
+
+Future<bool> _exitApp(BuildContext context) async {
+  if (await controllerGlobal.canGoBack()) {
+    print("onwill goback");
+    controllerGlobal.goBack();
+    return Future.value(true);
+  } else {
+    Scaffold.of(context).showSnackBar(
+      const SnackBar(content: Text("No back history item")),
+    );
+    return Future.value(false);
+  }
+}
+
 class WebViewLoadUI extends State<WebViewLoad>{
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body:
-      SafeArea(
-        child: WebView(
-          initialUrl: 'https://kanzulhaya.com/',
-          javascriptMode: JavascriptMode.unrestricted,
+    return WillPopScope(
+      onWillPop: () => _exitApp(context),
+      child: Scaffold(
+        body:
+        SafeArea(
+          child: WebView(
+            initialUrl: 'https://kanzulhaya.com/',
+            javascriptMode: JavascriptMode.unrestricted,
 
+          ),
         ),
       ),
     );
